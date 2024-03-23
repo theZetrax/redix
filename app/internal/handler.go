74485930fb24769c171346f5f35516f6784f85de
Zablon Dawit <zabjd22@gmail.com>
2024-03-23 17:11:45 +0300

    feat: remove the usage of the reader module

--- app/internal/handler.go
+++ app/internal/handler.go
@@ -11,16 +11,12 @@ type HttpHandler struct{}
 func (h *HttpHandler) HandleConnection(conn net.Conn) {
 	defer conn.Close()
 	buf := make([]byte, 1024)
-	len, err := conn.Read(buf)
+	_, err := conn.Read(buf)
 	if err != nil {
 		conn.Write([]byte("-ERR internal error\r\n"))
 	}
 
-	req := ParseRequest(buf[:len])
-	// log incoming request details
-	log.Println(req.Method, req.Url, req.HttpVersion)
-
-	_, err = conn.Write([]byte("+PONG" + CLRF))
+	_, err = conn.Write([]byte("+PONG\r\n"))
 	if err != nil {
 		log.Println("Error writing to connection: ", err.Error())
 		os.Exit(1)
