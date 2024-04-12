efa8809eda6a579d70f86a6ce43755326886eb49
Zablon Dawit <zabjd22@gmail.com>
2024-04-12 10:16:21 +0300

    feat: remove log

--- app/cmd/info.go
+++ app/cmd/info.go
@@ -1,15 +1,9 @@
 package cmd
 
 import (
-	"log"
-
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
 )
 
 func handleInfo(opts CMD_OPTS, _ []any) []byte {
-	response := resp.EncodeNodeInfo(*opts.ReplicaInfo)
-
-	log.Println("Response: ", string(response))
-
-	return response
+	return resp.EncodeNodeInfo(*opts.ReplicaInfo)
 }
